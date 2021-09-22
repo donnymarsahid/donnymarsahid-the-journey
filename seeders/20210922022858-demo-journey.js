e2e76610a134c696831db24ffa6abd226371e53c
donnymarsahid <donnymrshd.94@gmail.com>
2021-09-22 17:58:26 +0700

    updates client

--- seeders/20210922022858-demo-journey.js
+++ seeders/20210922022858-demo-journey.js
@@ -11,7 +11,7 @@ module.exports = {
         description:
           "After landing at the airport (coming from the sunshine coast) we were a little lost on how to work with the public transport. We soon realised that easily enough the train had a straight-line route to where we were",
         writer: "Veb",
-        image: "http://localhost:3001/images/journey-1",
+        image: "http://localhost:3001/images/journey-1.png",
         createdAt: "2021-06-22 05:04:32",
       },
       {
@@ -20,7 +20,7 @@ module.exports = {
         description:
           "Let’s start at the very beginning. The Tower of London (St Katharine's & Wapping; 020 3166 6000) takes you back to the London of William the Conqueror – it was around 1078 when he began work on ...",
         writer: "Oni-on",
-        image: "http://localhost:3001/images/journey-2",
+        image: "http://localhost:3001/images/journey-2.png",
         createdAt: "2021-07-22 05:04:32",
       },
       {
@@ -29,7 +29,7 @@ module.exports = {
         description:
           "Sang bagaskara sudah beberapa menit lalu berangkat ke peraduannya di ufuk sebelah barat. aku meninggalkan pantai kuta meski pengujung yang menikmati sunset masih belum berkurang, bahkan ...",
         writer: "Rizky",
-        image: "http://localhost:3001/images/journey-3",
+        image: "http://localhost:3001/images/journey-3.png",
         createdAt: "2021-08-22 05:04:32",
       },
       {
@@ -38,7 +38,7 @@ module.exports = {
         description:
           "Liburan di tahun baru 2020 keberangkatan saya menuju Pulau Dewata Bali.  Sampai lah saya malam itu di Bali Airport menujukan waktu jam 02.00, dan melanjutkan pejalanan yang menyenangkan..",
         writer: "Cipto",
-        image: "http://localhost:3001/images/journey-4",
+        image: "http://localhost:3001/images/journey-4.png",
         createdAt: "2021-09-22 05:04:32",
       },
     ]);
