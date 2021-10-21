c266bd1c649ec9d64af3de55045ff6fce75da66a
DON <donnymrshd.94@gmail..com>
2021-10-21 21:05:43 +0700

    updates deploy server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 
 app.use(cors());
